0d6c38271c339f51496a396e93e1ccb271045a89
matt-winzer <matt.winzer@gmail.com>
2019-04-22 10:16:07 -0600

    created GET ALL route for recipes with associations

--- index.js
+++ index.js
@@ -5,10 +5,31 @@ const PORT = process.env.PORT || 3000
 // TODO: add CORS
 // TODO: add bodyParser
 
+// Import Sequelize models
+const models = require('./models')
+
 app.get('/', (req, res, next) => {
   res.json({
     message: 'Server running!'
   })
+})
+
+app.get('/api/recipes', (req, res, next) => {
+  models.recipe.findAll({
+    include: [
+      { model: models.user },
+      'ingredients',
+      'instructions',
+      'tags'
+    ]
+  })
+    .then(recipes => {
+      res.json({ recipes })
+    })
+    .catch(error => {
+      console.log(error)
+      res.status(400).json({ message: 'Something went wrong...' })
+    })
 })
 
 // TODO: add error handling
