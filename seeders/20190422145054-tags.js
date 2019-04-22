e83da3a2a0f227ca7ef5699f8647d681b774ece4
matt-winzer <matt.winzer@gmail.com>
2019-04-22 08:53:46 -0600

    created seed data for tags

---
+++ seeders/20190422145054-tags.js
@@ -0,0 +1,50 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('People', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+    return queryInterface.bulkInsert('tags', [
+      {
+        name: 'italian',
+        img_url: 'https://placehold.it/200x200',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }, {
+        name: 'comfort-food',
+        img_url: 'https://placehold.it/200x200',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }, {
+        name: 'hot',
+        img_url: 'https://placehold.it/200x200',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }, {
+        name: 'healthy',
+        img_url: 'https://placehold.it/200x200',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('People', null, {});
+    */
+    return queryInterface.bulkDelete('tags', null, {});
+  }
+};
