d2982a2d0dd92d5fb005598f46c86f2cd15a8ec8
matt-winzer <matt.winzer@gmail.com>
2019-04-21 22:19:24 -0600

    created ingredient seed data

---
+++ seeders/20190422035647-ingredients.js
@@ -0,0 +1,84 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('People', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+    return queryInterface.bulkInsert('ingredients', [
+      {
+        name: 'cloves of garlic',
+        quantity: 2,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        recipeId: 1
+      }, {
+        name: 'tablespoons of olive oil',
+        quantity: 2,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        recipeId: 1
+      }, {
+        name: 'large white onions',
+        quantity: 3,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        recipeId: 1
+      }, {
+        name: 'pound of ground beef',
+        quantity: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        recipeId: 1
+      }, {
+        name: 'cans of cannellini',
+        quantity: 2,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        recipeId: 1
+      }, {
+        name: 'can of red kidney beans',
+        quantity: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        recipeId: 1
+      }, {
+        name: 'cans of pinto beans',
+        quantity: 2,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        recipeId: 1
+      }, {
+        name: 'small can of tomato paste',
+        quantity: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        recipeId: 1
+      }, {
+        name: 'cans of diced tomatoes',
+        quantity: 2,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        recipeId: 1
+      },
+    ], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('People', null, {});
+    */
+    return queryInterface.bulkDelete('ingredients', null, {});
+  }
+};
