6812d748f5a04e2e9b1236daca6e33c451f299de
matt-winzer <matt.winzer@gmail.com>
2019-04-22 08:50:29 -0600

    created migration for tag

---
+++ models/tag.js
@@ -0,0 +1,16 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const tag = sequelize.define('tag', {
+    name: DataTypes.STRING,
+    img_url: DataTypes.STRING
+  }, {});
+  tag.associate = function(models) {
+    // associations can be defined here
+    tag.belongsToMany(models.recipe, {
+      through: 'recipeTag',
+      foreignKey: 'tagId',
+      otherKey: 'recipeId'
+    })
+  };
+  return tag;
+};
