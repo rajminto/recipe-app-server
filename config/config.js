c0f7ea81b9e7afc499ebdc104c62b3c12e3cab2f
matt-winzer <matt.winzer@gmail.com>
2019-04-22 23:49:22 -0600

    removed config/config.json

--- config/config.js
+++ config/config.js
@@ -4,11 +4,6 @@ const fs = require('fs')
 module.exports = {
   development: {
     use_env_variable: 'DATABASE_URL',
-    "username": "matthewwinzer",
-    "password": null,
-    "database": "recipe_app",
-    "host": "127.0.0.1",
-    "dialect": "postgres"
   },
   test: {
     use_env_variable: 'DATABASE_URL'
