d7c0eb45002d6e339f1825d5d63d1e2903ecebb3
matt-winzer <matt.winzer@gmail.com>
2019-04-22 20:52:20 -0600

    refactored recipe controller to use error handling w/next

--- index.js
+++ index.js
@@ -22,7 +22,20 @@ app.get('/', (req, res, next) => {
 
 app.use('/api/recipes', recipesRouter)
 
-// TODO: add error handling
+// Error handling
+app.use(notFound)
+app.use(errorHandler)
+
+function notFound(req, res, next) {
+  res.status(404).send({ error: 'Not found!', status: 404, url: req.originalUrl })
+}
+
+// eslint-disable-next-line
+function errorHandler(err, req, res, next) {
+  console.error('ERROR', err)
+  const stack = process.env.NODE_ENV !== 'production' ? err.stack : undefined
+  res.status(500).send({ error: err.message, stack, url: req.originalUrl })
+}
 
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`))
 
