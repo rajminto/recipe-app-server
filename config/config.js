eb18783798a9dbe4c065a38a302d60d1087c8e7a
matt-winzer <matt.winzer@gmail.com>
2019-04-22 23:46:35 -0600

    added dotenv config to config.js

--- config/config.js
+++ config/config.js
@@ -1,8 +1,14 @@
-const fs = require('fs');
+require('dotenv').config()
+const fs = require('fs')
 
 module.exports = {
   development: {
-    use_env_variable: 'DATABASE_URL'
+    use_env_variable: 'DATABASE_URL',
+    "username": "matthewwinzer",
+    "password": null,
+    "database": "recipe_app",
+    "host": "127.0.0.1",
+    "dialect": "postgres"
   },
   test: {
     use_env_variable: 'DATABASE_URL'
