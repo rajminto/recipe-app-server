73c0a4bbd40a3864f4e2187997907a37d7196663
matt-winzer <matt.winzer@gmail.com>
2019-04-22 20:47:35 -0600

    added morgan for logging

--- index.js
+++ index.js
@@ -5,9 +5,15 @@ const PORT = process.env.PORT || 3000
 // TODO: add CORS
 // TODO: add bodyParser
 
+const morgan = require('morgan')
+
+// Middleware
+app.use(morgan(process.env.NODE_ENV !== 'production' ? 'dev' : 'combined'))
+
 // Routers
 const recipesRouter = require('./routes/recipes')
 
+// Routes
 app.get('/', (req, res, next) => {
   res.json({
     message: 'Server running!'
