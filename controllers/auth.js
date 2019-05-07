c75439fb113e732c077c0fd8a59106786362dc6a
matt-winzer <matt.winzer@gmail.com>
2019-05-07 16:03:32 -0600

    validated new user inputs

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,17 +1,23 @@
+// Sequelize models
+const User = require('../models').user
 
 
 const registerUser = (req, res, next) => {
   const { name, email, password, password2 } = req.body
   
+  // Validate user
   // TODO: improve validation
-  if (!validUser(req.body)) res.json({ message: 'Please enter a username, email, and a password longer than 5 characters.' })
-  else if (!validPasswords(password, password2)) res.json({ message: 'Please enter matching passwords.' })
+  if      (!validUser(req.body))                  res.json({ message: 'Please enter a username, email, and a password longer than 5 characters.' })
+  else if (!validPasswords(password, password2))  res.json({ message: 'Please enter matching passwords.' })
   else {
-    res.json(req.body)
-  }
+    // Validation passed
+    // Check if user already exists in db
+    
 
 }
 
+
+// ------------------------------ Vaidation Helpers ------------------------------
 
 function validUser({ name, email, password, password2 }) {
   const fieldsPresent = name && email && password && password2
