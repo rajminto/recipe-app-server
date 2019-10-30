b0b47b1f2afe136dc5a2144b0a6914105ad2bad0
matt-winzer <matt.winzer@gmail.com>
2019-10-29 18:34:26 -0600

    set default port to 4000

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 4000
 
 const morgan = require('morgan')
 const cors = require('cors')
