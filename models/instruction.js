44540bab36fe0d97f176280fd03106181b51691b
Matt Winzer <matt.winzer@gmail.com>
2020-07-11 16:02:01 -0600

    RRAS-33 Update models

--- models/instruction.js
+++ models/instruction.js
@@ -1,15 +1,19 @@
 'use strict'
 module.exports = (sequelize, DataTypes) => {
-  const instruction = sequelize.define('instruction', {
-    description: DataTypes.TEXT,
-    order: DataTypes.INTEGER,
-    recipeId: DataTypes.INTEGER
-  }, {})
-  instruction.associate = function(models) {
+  const instruction = sequelize.define(
+    'instruction',
+    {
+      description: DataTypes.TEXT,
+      order: DataTypes.INTEGER,
+      recipeId: DataTypes.INTEGER,
+    },
+    {}
+  )
+  instruction.associate = function (models) {
     // associations can be defined here
     instruction.belongsTo(models.recipe, {
       foreignKey: 'recipeId',
-      onDelete: 'CASCADE'
+      onDelete: 'CASCADE',
     })
   }
   return instruction
